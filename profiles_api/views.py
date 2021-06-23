5957fdc0194dd22484fa46976105c3d0bf79e7f8
merrazurizing <merrazuriz.ing@gmail.com>
2021-06-22 21:25:45 -0400

    Agregados post,put,patch,delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,14 @@ from django.http import response
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 # Create your views here.
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self , request ,format = None):
         """ Retornar lista de caracteristicas del APIView"""
@@ -16,3 +21,29 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'Hello' , 'an_apiview' : an_apiview})
+    
+    def post(self, request):
+        """ Crea un mensaje con nuestro nombre"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message= f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        """ Maneja actualizar un objeto"""
+        return Response({'method' : 'PUT'})
+
+    def patch(self,request,pk=None):
+        """ Maneja la actualizacion de un objeto"""
+        return Response({'method' : 'PATCH'})
+
+    def delete(self,request,pk=None):
+        """ Borrar un objeto"""
+        return Response({'method' : 'DELETE'})
